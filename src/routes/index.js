af60738cc9267ad0652d9224c6f6231940eeb2f5
Ramiro <ramiro.angel@radiumrocket.com>
2020-12-02 01:46:47 -0300

    boiler-types: added the code to the /routes/index.js file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
 const constructionCompanyRouter = require('./construction-company');
+const boilerTypesRouter = require('./boiler-types');
+
 const router = require('express').Router();
 
 router.use('/construction-company', constructionCompanyRouter);
+router.use('/boiler-types', boilerTypesRouter);
 
 module.exports = router;
