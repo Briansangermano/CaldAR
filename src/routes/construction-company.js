d54997629c733405120b85a04de287cf20591511
Ramiro <ramiro.angel@radiumrocket.com>
2020-12-04 01:06:03 -0300

    modified the endpoints -> wrote them correctly

--- src/routes/construction-company.js
+++ src/routes/construction-company.js
@@ -2,21 +2,27 @@ const constructionCompany = require('../controllers/construction-company.js');
 const router = require('express').Router();
 
 //Create a new Construction Company
-router.post('/addNew', constructionCompany.create);
+router.post('/', constructionCompany.create);
 
 //Update a Construction Company by id
-router.put('/updateById/:id_company', constructionCompany.update);
+router.put('/:id_company', constructionCompany.update);
+/* **** - router.put('/:id', constructionCompany.update); - **** --> I don't think this is correct because I have other
+endpoints with id first, as "id_building" and "id_user"*/
 
 //Delete a Construction Company by id
-router.delete('/deleteById/:id_company', constructionCompany.delete);
+router.delete('/:id_company', constructionCompany.delete);
+/* **** - router.delete('/:id', constructionCompany.delete); - **** --> I don't think this is correct because I have other
+endpoints with id first, as "id_building" and "id_user"*/
 
 //Retrieve all Construction Companies
-router.get('/getAll', constructionCompany.findAll);
+router.get('/', constructionCompany.findAll);
 
 //Retrieve Construction Companies by ID
-router.get('/getById/:id_company', constructionCompany.findOne);
+router.get('/:id_company', constructionCompany.findOne);
+/* **** - router.get('/:id', constructionCompany.findOne); - **** --> I don't think this is correct because I have other
+endpoints with id first, as "id_building" and "id_user"*/
 
 ////Retrieve Construction Companies by type (CUIT)
-router.get('/getByType/:cuit', constructionCompany.findCuit);
+router.get(':cuit', constructionCompany.findCuit);
 
 module.exports = router;
